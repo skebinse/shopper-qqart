e7e6b0251e4963d2858a2a8486c29ffdd0742082
skebinse <skebinse@gmail.com>
2023-01-13 00:06:47 +0900

    배치 완료 수정

--- pages/api/btch/btchCompList.js
+++ pages/api/btch/btchCompList.js
@@ -25,29 +25,18 @@ export default async function handler(req, res) {
                          , BB.SHOP_NM
                          , BB.SHOP_RRSN_ATCH_FILE_UUID
                          , CONCAT(BB.SHOP_ADDR, ' ' , BB.SHOP_DTPT_ADDR) AS SHOP_FULL_ADDR
-                         , IFNULL(DD.PROD_CNT, 0) AS PROD_CNT
                          , ST_DISTANCE_SPHERE(POINT(BB.SHOP_ADDR_LAT, BB.SHOP_ADDR_LOT), POINT(EE.SHPR_ADDR_LAT, EE.SHPR_ADDR_LOT)) AS SLIN_DTC
+                         , IFNULL((SELECT COUNT(*) FROM T_ODER_DTPT A1 WHERE A1.ODER_ID = AA.ODER_ID), 0) AS PROD_CNT
+                         , IFNULL((SELECT COUNT(*) FROM T_USER_SPBK A1 WHERE A1.ODER_USER_ID = AA.ODER_USER_ID), 0) AS SPBK_CNT
                       FROM T_ODER_USER_INFO AA
                            INNER JOIN T_SHOP_MAG BB
                         ON BB.SHOP_ID = AA.SHOP_ID
-                           LEFT OUTER JOIN
-                            (
-                            SELECT USER_ID
-                                 , SHOP_ID
-                                 , ODER_ID
-                                 , COUNT(SHOP_ID) AS PROD_CNT
-                              FROM T_ODER_DTPT
-                          GROUP BY USER_ID
-                                 , SHOP_ID
-                                 , ODER_ID
-                            ) DD
-                        ON AA.ODER_ID = DD.ODER_ID
                            INNER JOIN T_SHPR_INFO EE
                         ON EE.SHPR_ID = fnDecrypt(?, ?)
                        AND EE.SHPR_ID = AA.SHPR_ID
                      WHERE AA.ODER_DELY_CPL_DT IS NOT NULL
                    ) AA
-             WHERE (AA.PROD_CNT > 0 OR AA.ODER_KD = 'PIUP')
+             WHERE ((AA.PROD_CNT + AA.SPBK_CNT) > 0 OR AA.ODER_KD = 'PIUP')
           ORDER BY AA.ODER_DELY_CPL_DT DESC
                 `;
 

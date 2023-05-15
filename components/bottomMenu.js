4b462a7859ab2e0209f8d893162eaa4e4922f26f
skebinse <skebinse@gmail.com>
2023-05-15 10:15:42 +0900

    정산 메뉴로 변경

--- components/bottomMenu.js
+++ components/bottomMenu.js
@@ -8,13 +8,19 @@ export default function BottomMenu({idx}) {
             <ul>
                 <li className={idx === 0 ? 'on' : ''}>
                     <Link href={'/'}>
-                        <Image alt={'홈 아이콘'} src={`/assets/images/icon/iconHome${idx === 0 ? 'On' : 'Off'}.svg`} width={17} height={17} />
+                        <Image alt={'홈 아이콘'} src={`/assets/images/btn/btnMenuBtch${idx === 0 ? 'On' : ''}.svg`} width={24} height={24} />
                         <span>홈</span>
                     </Link>
                 </li>
                 <li className={idx === 1 ? 'on' : ''}>
+                    <Link href={'/mypage/btchAdj'}>
+                        <Image alt={'정산 아이콘'} src={`/assets/images/btn/btnMenuPay${idx === 1 ? 'On' : ''}.svg`} width={24} height={24} />
+                        <span>정산</span>
+                    </Link>
+                </li>
+                <li className={idx === 2 ? 'on' : ''}>
                     <Link href={'/mypage'}>
-                        <Image alt={'마이페이지 아이콘'} src={`/assets/images/icon/iconMypage${idx === 1 ? 'On' : 'Off'}.svg`} width={17} height={17} />
+                        <Image alt={'마이페이지 아이콘'} src={`/assets/images/btn/btnMenuMypage${idx === 2 ? 'On' : ''}.svg`} width={24} height={24} />
                         <span>마이페이지</span>
                     </Link>
                 </li>

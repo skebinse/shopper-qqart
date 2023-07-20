31432a5ffb0c5f4f695d38d8bfc1d550bd73f302
skebinse <skebinse@gmail.com>
2023-07-20 10:30:34 +0900

    현재 위치 저장

--- pages/api/cmm/insPsPsit.js
+++ pages/api/cmm/insPsPsit.js
@@ -12,11 +12,13 @@ export default async function handler(req, res) {
                      SHPR_ID
                    , SHPR_PSIT_LAT
                    , SHPR_PSIT_LOT
+                   , SHPR_PSIT_RGI_YMD
                    , RGI_DT
                 ) VALUES (
                      fnDecrypt(?, ?)
                    , ?
                    , ?
+                   , DATE_FORMAT(DATE_ADD(NOW(), INTERVAL 9 HOUR), '%Y-%m-%d')
                    , NOW()
                 )
             `;
